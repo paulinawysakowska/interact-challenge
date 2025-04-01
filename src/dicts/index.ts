3c5c592076a08545d777d0c048873eb76cba955f
paulina <paulina.wysakowska@gmail.com>
2025-04-01 18:13:40 +0200

    Add index.ts files

---
+++ src/dicts/index.ts
@@ -0,0 +1,6 @@
+export * from './addBlogPost.dict';
+export * from './homePage.dict';
+export * from './loginPage.dict';
+export * from './postPage.dict';
+export * from './publishPostDrawer.dict';
+export * from './userDrawer.dict';
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export * from './addBlogPost.page';
+export * from './homePage.page';
+export * from './login.page';
+export * from './postPage.page';
+export * from './publishPostDrawer.page';
+export * from './userDrawer.page';
